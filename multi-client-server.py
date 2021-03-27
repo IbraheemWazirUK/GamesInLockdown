94e55a076cba51240a895906ed23ca809f612507
skm20 <skm20@ic.a.cuk>
2021-03-27 19:12:52 +0000

    Added button presses in server

--- multi-client-server.py
+++ multi-client-server.py
@@ -2,11 +2,14 @@ import socket
 import os
 from _thread import *
 from datetime import datetime
+from pynput.keyboard import Key, Controller
 
 ServerSocket = socket.socket()
 host = '192.168.1.250'
 port = 5001
 ThreadCount = 0
+keyboard = Controller()
+
 try:
     ServerSocket.bind((host, port))
 except socket.error as e:
@@ -21,11 +24,16 @@ def threaded_client(connection):
     
     while True:
         data = connection.recv(2048)
-        reply = 'Server Says: ' + data.decode('utf-8')
-        print("Messaged recieved at: " + datetime.now().strftime("%H:%M:%S"))
+        reply = data.decode('utf-8')
+        validKeys = ['w', 'a', 's', 'd', 'g'] #Valid key presses
+        if reply[3] in validKeys:
+            print(f"Pressing {reply[3]}")
+            if reply[0] == 'p':
+                keyboard.press(reply[3])
+            elif reply[0] == 'r':
+                keyboard.release(reply[3])
         if not data:
             break
-        print(str.encode(reply))
     connection.close()
 
 while True:
